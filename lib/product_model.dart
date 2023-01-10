6bf0a5822dccb17fc6d74bd6c6999da4e2a777d3
AnaLarisa <ana.ivanov@student.upt.ro>
2023-01-10 21:02:38 +0200

    information returned correctly + translated

--- lib/product_model.dart
+++ lib/product_model.dart
@@ -1,21 +1,23 @@
-import 'package:translator/translator.dart';
+import 'dart:convert';
+import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
+
 import 'vegan_logic.dart';
 
-final translator = GoogleTranslator();
+Future<String> translate(String text, String fromLanguage) async {
+  var response = await http.get(Uri.parse(
+      'https://api.mymemory.translated.net/get?q=$text!&langpair=$fromLanguage|ro'));
 
-Future<String> translate(String text) async {
-  final translator = GoogleTranslator();
+  if (response.statusCode == 200) {
+    var data = json.decode(response.body);
 
-  await translator.translate(text, to: 'ro').then((response) {
-    return response.text;
-  }).onError((error, stackTrace) {
+    return data["responseData"] != null &&
+            data["responseData"]["translatedText"] != null
+        ? data["responseData"]["translatedText"]
+        : text;
+  } else {
     return text;
-  });
-  return "no se puede muchacho";
-}
-
-String getTranslatedString(Future<String> text) {
-  return Future.value(text).toString();
+  }
 }
 
 class Product {
@@ -27,32 +29,58 @@ class Product {
   String ingredientsNotVegan = "";
   String environmentalImpact = "";
   String nutriScore = "";
+  String language = "";
 
   Map<String, dynamic> data = {};
 
-  setFields() {
-    productName = setProductName();
-    ingredients = setIngredients();
-    ingredientString = setIngredientString();
+  Future setFields() async {
+    language = setLanguage();
+    productName = await setProductName();
+    ingredients = await setIngredients();
+    ingredientString = await setIngredientString();
     productImage = setProductImage();
     vegan = setVegan();
     ingredientsNotVegan = setIngredientsNotVegan();
-    environmentalImpact = setEnvironmentalImpact();
+    environmentalImpact = await setEnvironmentalImpact();
     nutriScore = setNutriScore();
   }
 
-  String setProductName() {
+  String setLanguage() {
+    return data["product"]["lang"] ?? "en";
+  }
+
+  Future<String> setProductName() async {
+    if (data["product"]["product_name"] != null) {
+      var productname =
+          await translate(data["product"]["product_name"], language);
+      return productname;
+    }
     return data["product"]["product_name"] ??
         "Numele produsului nu este disponibil";
   }
 
-  String setIngredientString() {
-    return data["product"]["ingredients_text"] ??
-        data["product"]["ingredients_text_debug"]
-            .toString()
-            .replaceAll('[', '')
-            .replaceAll(']', '') ??
-        "Lista de ingrediente nu este disponibilă.";
+  Future<List<String>> setIngredients() async {
+    var string = await translate(
+        data["product"]["ingredients_text_debug"].toString(), language);
+    return string
+        .split(RegExp('[(),]'))
+        .where((element) => element.length > 1)
+        .toList();
+  }
+
+  Future<String> setIngredientString() async {
+    if (data["product"]["ingredients_text"] != null) {
+      return await translate(data["product"]["ingredients_text"], language);
+    } else if (data["product"]["ingredients_text_debug"] != null) {
+      return await translate(
+          data["product"]["ingredients_text_debug"]
+              .toString()
+              .replaceAll('[', '')
+              .replaceAll(']', ''),
+          language);
+    } else {
+      return "Lista de ingrediente nu este disponibilă.";
+    }
   }
 
   String setProductImage() {
@@ -60,14 +88,6 @@ class Product {
         data["product"]["image_front_url"] ??
         data["product"]["image_front_small_url"] ??
         "";
-  }
-
-  List<String> setIngredients() {
-    return data["product"]["ingredients_text_debug"]
-        .toString()
-        .split(RegExp('[(),]'))
-        .where((element) => element.length > 1)
-        .toList();
   }
 
   String setIngredientsNotVegan() {
@@ -95,9 +115,10 @@ class Product {
         : false;
   }
 
-  String setEnvironmentalImpact() {
-    return data["product"]["packaging"] ??
-        "Nu exista date disponibile pentru impactul asupra mediului inconjurator";
+  Future<String> setEnvironmentalImpact() async {
+    return data["product"]["packaging"] != null
+        ? await translate(data["product"]["packaging"], language)
+        : "Nu exista date disponibile pentru impactul asupra mediului inconjurator";
   }
 
   String setNutriScore() {
@@ -109,16 +130,11 @@ class Product {
   }
 }
 
-Product getProduct(Map<String, dynamic> dataParsed) {
+Future<Product> getProduct(Map<String, dynamic> dataParsed) async {
   Product product = Product();
 
   product.data = dataParsed;
-  product.setFields();
+  await product.setFields();
 
   return product;
 }
-
-// Future<String> _getImage() async {
-//   await Future.delayed(const Duration(seconds: 1));
-//   return productImage;
-// }
